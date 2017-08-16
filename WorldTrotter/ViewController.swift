beb85d2e8ccbc3e975e206b458bf60dcb44e5ab3
SB <sb@SBs-MacBook-Pro.local>
2017-08-15 22:40:37 -0400

    removed practice code, added labels and Autolayout constraints

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -10,18 +10,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 10, y: 10, width: 80, height: 130)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.red
-        firstView.addSubview(secondView)
-    }
-
 }
 
