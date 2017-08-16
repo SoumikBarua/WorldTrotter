f06dc3ea6a028422eb428a3770cda9861404ba78
SB <sb@SBs-MacBook-Pro.local>
2017-08-15 21:26:26 -0400

    programmatically added views and subviews using CGRect frames

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -11,15 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 10, y: 10, width: 80, height: 130)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.red
+        firstView.addSubview(secondView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
