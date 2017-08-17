eeb3fe0ee0d6eeaafb0c146f3dc921790d36bf7d
SB <sb@SBs-MacBook-Pro.home>
2017-08-17 00:00:56 -0400

    replaced ViewController.swift with ConversionViewController.swift, implementing keyboard dismissal on tap and the actual degree conversion

---
+++ ConversionViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by SB on 8/16/17.
+//  Copyright © 2017 SB. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celciusLabel: UILabel!
+    @IBOutlet var fahrenheitTextField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>?{
+        didSet{
+            updateCelciusLabel()
+        }
+    }
+    var celciusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelciusLabel(){
+        if let celciusValue = celciusValue {
+            celciusLabel.text = "\(celciusValue)"
+        } else {
+            celciusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        //if let text = textField.text, !text.isEmpty {
+        //    celciusLabel.text = textField.text
+        //} else {
+        //    celciusLabel.text = "???"
+        //}
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        fahrenheitTextField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelciusLabel()
+    }
+    
+}
