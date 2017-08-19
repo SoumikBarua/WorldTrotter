c24697454153e829d79574789fd9ffdbc4753a32
SB <sb@dyn-129-236-235-148.dyn.columbia.edu>
2017-08-19 18:18:23 -0400

    overrode viewDidLoad() in the two view controllers to spot lazy loading, and implemented a simple background color change with two colors each time the ConversionViewController is viewed

--- ConversionViewController.swift
+++ ConversionViewController.swift
@@ -13,6 +13,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celciusLabel: UILabel!
     @IBOutlet var fahrenheitTextField: UITextField!
     
+    var colorOption = 0
+    
     var fahrenheitValue: Measurement<UnitTemperature>?{
         didSet{
             updateCelciusLabel()
@@ -56,6 +58,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         
         updateCelciusLabel()
+        
+        print("ConversionViewController loaded its view.")
     }
     
     let numberFormatter: NumberFormatter = {
@@ -68,11 +72,13 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
+        /// Disallow entry of alphabetic characters in the fahrenheit text field
         let characterSet = NSCharacterSet.letters
         if (string.rangeOfCharacter(from: characterSet) != nil) {
             return false
         }
         
+        /// Disallow usage of more than one decimals in the fahrenheit text field
         let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
         let replacementTextHasDecimalSeparator = string.range(of: ".")
         
@@ -80,6 +86,18 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
             return false
         } else {
             return true
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if colorOption == 0{
+            view.backgroundColor = UIColor(red:0.00, green:0.26, blue:0.26, alpha:1.0)
+            colorOption = 1
+        } else {
+            view.backgroundColor = UIColor(red:0.96, green:0.96, blue:0.95, alpha:1.0)
+            colorOption = 0
         }
     }
 
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by SB on 8/18/17.
+//  Copyright © 2017 SB. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
